04069884d199c602490dbb5349f5a93a6e2e1a5c
Pushkar Joshi <psjoshi@indiana.edu>
2013-12-08 01:20:45 -0500

    Adding service detection.

---
+++ service.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<stdio.h>
+#include<vector>
+#include <getopt.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fstream>
+#include<string>
+
+using namespace std;
+
+string servChk(string ipToScan, unsigned short port);
+
